c2a9de4dbb87040e59557be796aea177be6f27bc
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-04-18 09:02:52 -0300

    feat: normal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
-
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className="dark">
+    <html lang="pt-br">
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
+        {children}
       </body>
     </html>
   );
