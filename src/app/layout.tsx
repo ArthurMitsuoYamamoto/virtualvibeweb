bbf376496800deffc9335f2484a9967e145a8ae9
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-04-18 09:00:35 -0300

    feature: provider add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className="dark">
       <body className={inter.className}>
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
